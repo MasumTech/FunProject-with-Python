ee0a76061a56acb6cd987ea4ef1bbd6d49eb69d3
Masum@ishraak <masum@ishraak.com>
2020-03-07 10:34:12 +0600

    Twister game in Python

---
+++ twister_game.py
@@ -0,0 +1,9 @@
+import random
+
+list_colors = ['red', 'blue', 'yellow', 'green']
+list_bodyparts = ['right_hand', 'left_hand', 'right_leg', 'left_leg']
+
+test1 = random.choice(list_colors)
+test2 = random.choice(list_bodyparts)
+
+print(test1, test2)
