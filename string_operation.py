2fe07066290c7a20d82c5ba1b2c7e0de75b0bdcc
Masum@ishraak <masum@ishraak.com>
2020-03-07 09:52:20 +0600

    string operation in python

---
+++ string_operation.py
@@ -0,0 +1,9 @@
+name = input('Please enter your name: ')
+number = input('Please enter your number here: ')
+
+
+sqr_number = int(number) ** 2
+
+result = f'Hi, {name} you have entered number {number} whice has squre value of {sqr_number}'
+
+print(result)
